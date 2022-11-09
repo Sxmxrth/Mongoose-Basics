c19ea31b57716d8dbd0f6e424a1ba25d1e433485
Sxmxrth <samarthgupta1202@gmail.com>
2022-11-09 10:37:49 +0530

    complete CRUD

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+//COMPLETE CRUD WITH MONGOOSE
 const mongoose = require("mongoose");
 
 mongoose.connect("mongodb://0.0.0.0:27017/fruitsDB", {useNewUrlParser : true});
 
+//creation of schema
 const fruitSchema = new mongoose.Schema({
     name : String,
     rating : Number,
@@ -9,8 +11,11 @@ const fruitSchema = new mongoose.Schema({
 
 })
 
+//creation of collection
 const Fruit = mongoose.model("Fruit", fruitSchema);
 
+
+//creation of document(single entry)
 const apple = new Fruit({
     name : "Apple",
     rating : 8,
@@ -37,13 +42,24 @@ const banana = new Fruit({
 //     }
 // })
 
+//update operation
+Fruit.updateOne({_id : "636b2ae17925aeeb856d0f49"}, {name : "Peach"}, function(err){
+    err ? console.log(err) : console.log("successfully updated");
+})
+
+//delete operation
+Fruit.deleteOne({_id : "636b2ae17925aeeb856d0f4a"}, function(err){
+    err ? console.log(err) : console.log("successfully deleted kiwi");
+})
+
+//read operation
 Fruit.find(function(err, fruit){
     if(err){
         console.log(err);
     }
     else{
         
-        mongoose.connection.close();
+        // mongoose.connection.close();
 
         fruit.forEach(function(singleFruit){
             console.log(singleFruit.name);
@@ -55,7 +71,8 @@ Fruit.find(function(err, fruit){
 
 const personSchema = new mongoose.Schema({
     name : String,
-    age : Number
+    age : Number,
+    favouriteFruit : fruitSchema
 })
 
 const Person = mongoose.model("Person", personSchema);
@@ -65,7 +82,11 @@ const person = new Person({
     age : 20
 })
 
-//person.save();
+person.updateOne({name : "Samarth"}, {favouriteFruit : banana}, function(err){
+    err ? console.log(err) : console.log("successfully updated");
+})
+
+person.save();
 
 
 
